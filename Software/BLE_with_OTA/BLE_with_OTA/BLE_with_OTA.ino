48c8d2b41f49a9465da20141de7c54f38ebb9eb9
theBrutzler <spankysmail@gmx.de>
2021-10-05 14:13:12 +0200

    Solved #1
    #1 is now fixed for german keyboard

--- Software/BLE_with_OTA/BLE_with_OTA/BLE_with_OTA.ino
+++ Software/BLE_with_OTA/BLE_with_OTA/BLE_with_OTA.ino
@@ -41,10 +41,10 @@ void setup() {
     Key_Kommand[i] = 0;
   }
  
- Key_Kommand[0] = KEY_UP_ARROW;
+  Key_Kommand[0] = KEY_UP_ARROW;
   Key_Kommand[1] = '?';
-  Key_Kommand[3] = 0xF6;//'ö';
-  Key_Kommand[4] = 0x3C;//'<';
+  Key_Kommand[3] = 187;// 0xF6;//'ö';
+  Key_Kommand[4] = 236 ;//0x3C;//'<';
   Key_Kommand[5] = '1'; 
   Key_Kommand[6] = '4';
   Key_Kommand[7] = '7';
@@ -72,27 +72,27 @@ void setup() {
   Key_Kommand[96] = '?';
   Key_Kommand[97] = KEY_F12;
   Key_Kommand[98] = 'l';
-  Key_Kommand[99] = 0x2D;//'-';
+  Key_Kommand[99] = 192;// 0x2D;//'-';
   Key_Kommand[100] = KEY_PAGE_DOWN; 
   Key_Kommand[101] = KEY_PAGE_UP; 
   Key_Kommand[102] = KEY_END;
   Key_Kommand[114] = KEY_TAB;
   Key_Kommand[115] = 'u';
   Key_Kommand[116] = KEY_F8;
-  Key_Kommand[117] = 0x2D;//'-';  
+  Key_Kommand[117] = 181;//0x2D;//'-';  
   Key_Kommand[118] = 'n';
   Key_Kommand[119] = 'h';
   Key_Kommand[120] = '8';
-  Key_Kommand[121] = 0xFC;//'ü';
+  Key_Kommand[121] = 183;// 0xFC;//'ü';
   Key_Kommand[133] = KEY_RIGHT_ARROW;
-  Key_Kommand[134] = 0x2D;//'-'; 
+  Key_Kommand[134] = 192;//0x2D;//'-'; 
   Key_Kommand[135] = 't';
   Key_Kommand[136] = KEY_BACKSPACE;
   Key_Kommand[137] = KEY_CAPS_LOCK;
   Key_Kommand[138] = KEY_RETURN;
-  Key_Kommand[140] = 0x2B;//'+';
+  Key_Kommand[140] = 184;// 0x2B;//'+';
   Key_Kommand[152] = KEY_DOWN_ARROW; 
-  Key_Kommand[153] = 0x2A;//'*';
+  Key_Kommand[153] = 125 ;//0x2A;//'*';
   Key_Kommand[154] = 'r';
   Key_Kommand[155] = 'j';
   Key_Kommand[157] = '3';
@@ -102,7 +102,7 @@ void setup() {
   Key_Kommand[174] = KEY_F4; 
   Key_Kommand[175] = KEY_RIGHT_GUI;
   Key_Kommand[190] = KEY_LEFT_ARROW; 
-  Key_Kommand[191] = 0x2F;//'/';
+  Key_Kommand[191] = 38;//0x2F;//'/';
   Key_Kommand[192] = KEY_ESC;
   Key_Kommand[193] = 'k';
   Key_Kommand[194] = '0'; 
@@ -115,7 +115,7 @@ void setup() {
   Key_Kommand[212] = KEY_F2;
   Key_Kommand[213] = ',';
   Key_Kommand[215] = '0';
-  Key_Kommand[216] = 0x23;//'#';
+  Key_Kommand[216] = 185 ;//0x23;//'#';
   Key_Kommand[230] = 'y';
   Key_Kommand[231] = '1';
   Key_Kommand[235] = KEY_RIGHT_ALT;
@@ -129,15 +129,15 @@ void setup() {
   Key_Kommand[285] = 'd';
   Key_Kommand[286] = 'i'; 
   Key_Kommand[287] = KEY_F9;  
-  Key_Kommand[288] = 0x60;//'´';
+  Key_Kommand[288] = 61;//  0x60;//'´';---------------
   Key_Kommand[289] = KEY_RIGHT_GUI; 
-  Key_Kommand[290] = 0xE4;//'ä';
+  Key_Kommand[290] = 188;// 0xE4;//'ä';
   Key_Kommand[291] = '9';
-  Key_Kommand[292] = 0x2B;//'+'; 
+  Key_Kommand[292] = 184;//0x2B;//'~';
   Key_Kommand[304] = 's';
   Key_Kommand[305] = 'z';
   Key_Kommand[306] = KEY_F7;
-  Key_Kommand[307] = 0x5E;//'^';
+  Key_Kommand[307] = 96 ;//0x5E;//'^';---------------
   Key_Kommand[310] = '7';
   Key_Kommand[311] = 'p';
   Key_Kommand[323] = KEY_F5;
@@ -150,10 +150,7 @@ void setup() {
   Key_Kommand[345] = '6';
   Key_Kommand[348] = KEY_LEFT_SHIFT;
 
-  Serial.begin(115200);
-
   bleKeyboard.begin();
- ArduinoOTA.setHostname("BLE_Keyboard");
     ArduinoOTA
       .onStart([]() {
         String type;
@@ -163,21 +160,21 @@ void setup() {
           type = "filesystem";
   
         // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-        Serial.println("Start updating " + type);
+        //Serial.println("Start updating " + type);
       })
       .onEnd([]() {
-        Serial.println("\nEnd");
+        //Serial.println("\nEnd");
       })
       .onProgress([](unsigned int progress, unsigned int total) {
-        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+        //Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
       })
       .onError([](ota_error_t error) {
-        Serial.printf("Error[%u]: ", error);
-        if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-        else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-        else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-        else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-        else if (error == OTA_END_ERROR) Serial.println("End Failed");
+        //Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR) ;//Serial.println("Auth Failed");
+        else if (error == OTA_BEGIN_ERROR) ;//Serial.println("Begin Failed");
+        else if (error == OTA_CONNECT_ERROR) ;//Serial.println("Connect Failed");
+        else if (error == OTA_RECEIVE_ERROR) ;//Serial.println("Receive Failed");
+        else if (error == OTA_END_ERROR) ;//Serial.println("End Failed");
         ESP.restart();
       });
 
@@ -192,6 +189,7 @@ void setup() {
 
 // the loop routine runs over and over again forever:
 void loop() {
+  
   if (bleKeyboard.isConnected()) {
     for (int i = 0; i < 19; i++) {
       digitalWrite(Col[i], HIGH);
@@ -204,35 +202,35 @@ void loop() {
            // Serial.print(":True ");
             if (Key[i * 19 + y] == false) {
               bleKeyboard.press(Key_Kommand[i * 19 + y]);
-              Serial.print("Press: ");
-              Serial.println(Key_Kommand[i * 19 + y]);
+              //Serial.print("Press: ");
+              //Serial.println(Key_Kommand[i * 19 + y]);
               Key[i * 19 + y] = true;
             }
           }
           else {
             if (Key[i * 19 + y] == true) {
               bleKeyboard.release(Key_Kommand[i * 19 + y]);
-              Serial.print("Release: ");
-              Serial.println(Key_Kommand[i * 19 + y]);
+              //Serial.print("Release: ");
+              //Serial.println(Key_Kommand[i * 19 + y]);
               Key[i * 19 + y] = false;
             }
           }
           if(i==15&&y==4&&Key[i * 19 + y] == true){
-            Serial.println("KEY_RIGHT_GUI_Pressed");
+            //Serial.println("KEY_RIGHT_GUI_Pressed");
             if(OTA_STAGE_1 == false){
               first_try_millis=millis();
               OTA_STAGE_1=true;
-              Serial.println("OTA_STAGE_1 SET");
+              //Serial.println("OTA_STAGE_1 SET");
             }
             seacond_try_millis=millis();
             if(OTA_STAGE_1==true&&((seacond_try_millis-first_try_millis)>=timeout)){
-              Serial.println("OTA SET");
+              //Serial.println("OTA SET");
               OTA=true;
             }
           }
           if(i==15&&y==4&&(Key[i * 19 + y] == false)&&(OTA_STAGE_1==true)){
             OTA_STAGE_1=false;
-            Serial.println("OTA_STAGE_1 RESET");
+            //Serial.println("OTA_STAGE_1 RESET");
           }
         }
         digitalWrite(Col[i], LOW);
@@ -250,15 +248,15 @@ void loop() {
       WiFi.begin(ssid, password);
       delay(1000);
       while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-        Serial.println("Connection Failed! Rebooting...");
+        //Serial.println("Connection Failed! Rebooting...");
         delay(5000);
         ESP.restart();
       }
       ArduinoOTA.begin();
       
-      Serial.println("Ready");
-      Serial.print("IP address: ");
-      Serial.println(WiFi.localIP());
+      //Serial.println("Ready");
+      //Serial.print("IP address: ");
+      //Serial.println(WiFi.localIP());
       OTA_INIT=true;
     }
     ArduinoOTA.handle();
